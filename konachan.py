98e602fb88bd9e76dbe0c9601d473095bd281956
fateorder <feastwang@icloud.com>
2016-04-15 23:42:52 +0800

    Added files via upload

---
+++ konachan.py
@@ -0,0 +1,38 @@
+#-*-coding:utf-8-*-
+import urllib2
+import urllib
+import json
+import time
+import os
+def main(tag, page, limit):
+	# 获得数据
+	url = 'https://konachan.net/post.json?tag=%r?page=%r?limit=%r' % (tag, page, limit)
+	req = urllib2.Request(url)
+	r = urllib2.urlopen(req)
+	
+	#处理数据
+	imagelist = []	
+	result = json.loads(r.read())
+	for i in range(len(result)):
+		imagelist.append(result[i]['jpeg_url'])
+
+	#创建目录
+	if not os.path.exists(tag):
+		os.makedirs(tag)
+
+	#下载图片并保存
+	n = len(imagelist)
+	for j in range(1):
+		print imagelist[j]
+		filename = "%s/%s_%s.jpeg" % (tag, page, j)
+		urllib.urlretrieve(url=imagelist[j],filename=filename)
+
+	exit()
+
+if __name__ == '__main__':
+	#输入参数
+	tag = 'asuka'
+	page = 1 
+	limit = 1
+	#主函数运行
+	main(tag, page, limit)
